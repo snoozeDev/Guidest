2d83b736846219f926dfa64da8492ddcb1ff04e3
Jb Lafon <jb.lechevalier.lafon@gmail.com>
2019-03-04 19:32:48 +0100

    finished sass gulp task, need to to css minification next

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var plumber = require('gulp-plumber');
+// var plumber = require('gulp-plumber');
 // var watch = require( 'gulp-watch' );
+// var gulpSequence = require('gulp-sequence');
+// var sourcemaps = require( 'gulp-sourcemaps' );
+// var cleanCSS = require( 'gulp-clean-css' );
+var autoprefixer = require( 'gulp-autoprefixer' );
 
 var cfg = require( './gulpconfig.json' );
 var paths = cfg.paths;
 
 gulp.task( 'sass', function() {
     return stream = gulp.src( paths.sass + '/*.scss' )
-        .pipe( plumber( {
-            errorHandler: function( err ) {
-                console.log( err );
-                this.emit( 'end' );
-            }
-        } ) )
         .pipe( sass( { errLogToConsole: true } ) )
         .pipe( autoprefixer( 'last 2 versions' ) )
         .pipe( gulp.dest( paths.css ) );
@@ -21,5 +19,25 @@ gulp.task( 'sass', function() {
 });
 
 gulp.task( 'watch', function() {
-    gulp.watch( paths.sass + '/**/*.scss', ['styles'] );
+    gulp.watch( paths.sass + '/*.scss', gulp.series('sass') );
 });
+
+// TODO: MinifyCSS Task
+// gulp.task( 'minifycss', function() {
+//     return gulp.src( paths.css + '/style.css' )
+//         .pipe( sourcemaps.init( { loadMaps: true } ) )
+//         .pipe( cleanCSS( { compatibility: '*' } ) )
+//         .pipe( plumber( {
+//             errorHandler: function( err ) {
+//                 console.log( err ) ;
+//                 this.emit( 'end' );
+//             }
+//         } ) )
+//         .pipe( rename( { suffix: '.min' } ) )
+//         .pipe( sourcemaps.write( './' ) )
+//         .pipe( gulp.dest( paths.css ) );
+// });
+//
+// gulp.task( 'styles', function( callback ) {
+//     gulpSequence( 'sass')( callback );
+// } );
