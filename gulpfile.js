945e8dc508726e2444cc1b30f6a9109855b25fce
Jb Lafon <jb.lechevalier.lafon@gmail.com>
2019-03-05 10:51:12 +0100

    added minifycss task + jquery

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-// var plumber = require('gulp-plumber');
+var plumber = require('gulp-plumber');
 // var watch = require( 'gulp-watch' );
 // var gulpSequence = require('gulp-sequence');
-// var sourcemaps = require( 'gulp-sourcemaps' );
-// var cleanCSS = require( 'gulp-clean-css' );
+var sourcemaps = require( 'gulp-sourcemaps' );
+var cleanCSS = require( 'gulp-clean-css' );
 var autoprefixer = require( 'gulp-autoprefixer' );
+var rename = require( 'gulp-rename' );
 
 var cfg = require( './gulpconfig.json' );
 var paths = cfg.paths;
 
+// Sass task
 gulp.task( 'sass', function() {
     return stream = gulp.src( paths.sass + '/*.scss' )
         .pipe( sass( { errLogToConsole: true } ) )
@@ -18,26 +20,17 @@ gulp.task( 'sass', function() {
     //.pipe( rename( 'custom-editor-style.css' ) );
 });
 
-gulp.task( 'watch', function() {
-    gulp.watch( paths.sass + '/*.scss', gulp.series('sass') );
+// MinifyCSS Task
+gulp.task( 'minifycss', function() {
+    return gulp.src( paths.css + '/style.css' )
+        .pipe( sourcemaps.init( { loadMaps: true } ) )
+        .pipe( cleanCSS( { compatibility: '*' } ) )
+        .pipe( rename( { suffix: '.min' } ) )
+        .pipe( sourcemaps.write( './' ) )
+        .pipe( gulp.dest( paths.css ) );
 });
 
-// TODO: MinifyCSS Task
-// gulp.task( 'minifycss', function() {
-//     return gulp.src( paths.css + '/style.css' )
-//         .pipe( sourcemaps.init( { loadMaps: true } ) )
-//         .pipe( cleanCSS( { compatibility: '*' } ) )
-//         .pipe( plumber( {
-//             errorHandler: function( err ) {
-//                 console.log( err ) ;
-//                 this.emit( 'end' );
-//             }
-//         } ) )
-//         .pipe( rename( { suffix: '.min' } ) )
-//         .pipe( sourcemaps.write( './' ) )
-//         .pipe( gulp.dest( paths.css ) );
-// });
-//
-// gulp.task( 'styles', function( callback ) {
-//     gulpSequence( 'sass')( callback );
-// } );
+// Run gulp watch
+gulp.task( 'watch', function() {
+    return gulp.watch( paths.sass + '/*.scss', gulp.series('sass', 'minifycss') );
+});
