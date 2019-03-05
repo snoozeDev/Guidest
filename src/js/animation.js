781f7805b95811c6cfdc78f769afdc7e4c08201f
Jb Lafon <jb.lechevalier.lafon@gmail.com>
2019-03-05 14:29:20 +0100

    started animations

---
+++ src/js/animation.js
@@ -0,0 +1,14 @@
+// ===================================
+// Project: Voyages-Corsaire
+// File: animation.js
+// Author: Jb
+// Date: 2019-03-05
+// Description: File for JS animations
+// ===================================
+
+// Mark: Scroll Magic
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build scene
+var scene = new ScrollMagic.Scene();
--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,2 +1,10 @@
+// ===================================
+// Project: Voyages-Corsaire
+// File: custom-javascript.js
+// Author: Jb
+// Date: 2019-03-05
+// Description: Main JS file
+// ===================================
+
 // Hide 000webhost banner
 $("a[title~='Hosted']").parent().hide();
