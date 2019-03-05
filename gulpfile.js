5f1ae93a61d1c9c9adbc31cc590336e8209639c7
Jb Lafon <jb.lechevalier.lafon@gmail.com>
2019-03-05 11:57:29 +0100

    finilized script gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var sourcemaps = require( 'gulp-sourcemaps' );
 var cleanCSS = require( 'gulp-clean-css' );
 var autoprefixer = require( 'gulp-autoprefixer' );
 var rename = require( 'gulp-rename' );
+var concat = require( 'gulp-concat' );
+var uglify = require( 'gulp-uglify' );
 
 var cfg = require( './gulpconfig.json' );
 var paths = cfg.paths;
@@ -30,7 +32,32 @@ gulp.task( 'minifycss', function() {
         .pipe( gulp.dest( paths.css ) );
 });
 
+// TODO Minifyjs
+gulp.task( 'scripts', async function() {
+    var scripts = [
+
+        // Start - All BS4 stuff
+        paths.dev + '/js/bootstrap.js',
+
+        // End - All BS4 stuff
+
+        // Adding currently empty javascript file to add on for your own themes´ customizations
+        // Please add any customizations to this .js file only!
+        paths.dev + '/js/custom-javascript.js'
+    ];
+    gulp.src( scripts )
+        .pipe( concat( 'scripts.min.js' ) )
+        .pipe( uglify() )
+        .pipe( gulp.dest( paths.js ) );
+
+    gulp.src( scripts )
+        .pipe( concat( 'scripts.js' ) )
+        .pipe( gulp.dest( paths.js ) );
+
+});
+
 // Run gulp watch
 gulp.task( 'watch', function() {
-    return gulp.watch( paths.sass + '/*.scss', gulp.series('sass', 'minifycss') );
+    gulp.watch( paths.sass + '/*.scss', gulp.series('sass', 'minifycss') );
+    gulp.watch( [paths.devjs + '/*.js'], gulp.series('scripts') );
 });
