0eb1fc9f515790009fa7d0f779abd5bce492a17e
Jb Lafon <jb.lechevalier.lafon@gmail.com>
2019-03-08 14:28:12 +0100

    CTA focus fix

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -4441,3 +4441,9 @@
 // Date: 2019-03-05
 // Description: Main JS file
 // ===================================
+
+var button = $(".cta-btn");
+button.click(function () {
+    delay(200);
+    button.trigger("blur");
+});
--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -5,3 +5,9 @@
 // Date: 2019-03-05
 // Description: Main JS file
 // ===================================
+
+var button = $(".cta-btn");
+button.click(function () {
+    delay(200);
+    button.trigger("blur");
+});
