dd19a675c3a34ec36c2f21f677a2c9f6328f42e9
Jb Lafon <jb.lechevalier.lafon@gmail.com>
2019-03-11 19:19:55 -0400

    JS on dcoument ready

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -6,28 +6,30 @@
 // Description: Main JS file
 // ===================================
 
-//AOS
-AOS.init();
+$(document).ready(function () {
+    //AOS
+    AOS.init();
 
-// Video Parallax
-$('.jarallax').jarallax({
-    speed: 0.2
+    // Video Parallax
+    $('.jarallax').jarallax({
+        speed: 0.2
+    });
+
+    // Countdown timer
+    var countDownDate = new Date("Apr 18, 2021 12:00:00").getTime();
+    var x = setInterval(function () {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "EXPIRED";
+        }
+    }, 1000);
 });
-
-// Countdown timer
-var countDownDate = new Date("Apr 18, 2021 12:00:00").getTime();
-var x = setInterval(function () {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("countdown").innerHTML = "EXPIRED";
-    }
-}, 1000);
