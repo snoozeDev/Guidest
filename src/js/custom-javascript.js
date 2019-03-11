2d5b111b1fff24e576f8bb15305cb43bbc6e9403
Jb Lafon <jb.lechevalier.lafon@gmail.com>
2019-03-11 18:32:33 -0400

    parallax test

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -5,9 +5,26 @@
 // Date: 2019-03-05
 // Description: Main JS file
 // ===================================
-
-var button = $(".cta-btn");
-button.click(function () {
-    delay(200);
-    button.trigger("blur");
+// Video Parallax
+$('.jarallax').jarallax({
+    speed: 0.1
 });
+
+// Countdown timer
+
+// var countDownDate = new Date("Apr 18, 2021 12:00:00").getTime();
+// var x = setInterval(function () {
+//     var now = new Date().getTime();
+//     var distance = countDownDate - now;
+//     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//
+//     document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+//
+//     if (distance < 0) {
+//         clearInterval(x);
+//         document.getElementById("countdown").innerHTML = "EXPIRED";
+//     }
+// }, 1000);
